9c7a1a8c3a19469aaec88c4d5cf619ac7609c993
Benjamin Régnier <bregnier@jin.fr>
2021-05-26 10:15:13 +0200

    Initialize Backend project

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+import app from "./main"
+import http from "http"
+
+const port = process.env.PORT || "3000"
+app.set("port", port)
+const server = http
+  .createServer(app)
+  .listen(port)
+  .on("error", (error) => {
+    if (error.syscall !== "listen") {
+      throw error
+    }
+
+    var bind = typeof port === "string" ? "Pipe " + port : "Port " + port
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges")
+        process.exit(1)
+      case "EADDRINUSE":
+        console.error(bind + " is already in use")
+        process.exit(1)
+      default:
+        throw error
+    }
+  })
+  .on("listening", () => {
+    const addr = server.address()
+    const bind =
+      typeof addr === "string" ? "pipe " + addr : "port " + addr?.port
+    console.log(`Listening on ${bind}`)
+  })
---
+++ backend/src/main.js
@@ -0,0 +1,25 @@
+import createError from "http-errors"
+import express from "express"
+import cookieParser from "cookie-parser"
+import logger from "morgan"
+
+const app = express()
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
+
+  res.status(err.status || 500)
+  res.render("error")
+})
+
+export default app
