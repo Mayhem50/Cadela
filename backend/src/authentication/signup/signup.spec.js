7f21ddb8ace85f73c1f4547343c86f08048e5a1c
Benjamin Régnier <bregnier@jin.fr>
2021-05-27 09:28:27 +0200

    Make save user and return a token

--- backend/src/authentication/signup/signup.spec.js
+++ backend/src/authentication/signup/signup.spec.js
@@ -1,5 +1,32 @@
+import { jest } from "@jest/globals"
+
+const makeUserRepository = () => {
+  const save = jest.fn((user) => {
+    return "any_token"
+  })
+  return { save }
+}
+
+const userRepository = makeUserRepository()
+
+const makeSignupService = (userRepository) => {
+  const signup = (user) => {
+    const token = userRepository.save(user)
+    return {
+      body: { token }
+    }
+  }
+
+  return { signup }
+}
+
 describe("Signup", () => {
-  it("Save credential", () => {
-    expect(credentialRepository).toHaveBeenCalledWith(credential)
+  it("Save user and return token", async () => {
+    const signupService = makeSignupService(userRepository)
+    const user = {}
+    const ret = signupService.signup(user)
+
+    expect(userRepository.save).toHaveBeenCalledWith(user)
+    expect(ret.body).toHaveProperty("token")
   })
 })
