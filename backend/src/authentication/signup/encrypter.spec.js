07309dbf8cb99f0bb4f5bc345d874df7c5850b26
Benjamin Régnier <bregnier@jin.fr>
2021-05-27 17:43:05 +0200

    Move encrypter to its file

--- backend/src/authentication/signup/encrypter.spec.js
+++ backend/src/authentication/signup/encrypter.spec.js
@@ -1,17 +1,5 @@
-import bcrypt from "bcryptjs"
 import { EncrypterContract } from "./encrypter.contract"
-
-const makeEncrypter = () => {
-  const encrypt = async (password) => {
-    return bcrypt.hashSync(password, 10)
-  }
-
-  const compare = async (password, hash) => {
-    return bcrypt.compareSync(password, hash)
-  }
-
-  return { encrypt, compare }
-}
+import { makeEncrypter } from "./encrypter"
 
 describe("BcryptJs Encrypter", () => {
   EncrypterContract(makeEncrypter())
