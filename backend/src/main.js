ecbe71030dd3110335f9da6e83f59c9dda5c2927
Benjamin Régnier <bregnier@jin.fr>
2021-05-27 09:17:43 +0200

    Add base for signup

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,12 +3,16 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import logger from "morgan"
 
+import { router } from "./router"
+
 const app = express()
 
 app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+app.use("/api", router)
 
 app.use((req, res, next) => {
   next(createError(404))
