309b18a7b33ef3dadf1cbd798429d758efb5bac1
Benjamin Régnier <bregnier@jin.fr>
2021-05-27 15:37:40 +0200

    use env var for JWT secret

--- backend/src/authentication/signup/token-generator.js
+++ backend/src/authentication/signup/token-generator.js
@@ -2,7 +2,7 @@ import Jwt from "jsonwebtoken"
 
 export const makeTokenGenerator = () => {
   const generate = (userId) => {
-    return Jwt.sign(userId, "jwt_secret")
+    return Jwt.sign(userId, process.env.JWT_SECRET)
   }
 
   return { generate }
