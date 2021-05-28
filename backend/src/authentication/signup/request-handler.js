3705486e154abb1eeb391601924c4ef18406bc65
Benjamin Régnier <bregnier@jin.fr>
2021-05-28 09:22:23 +0200

    Fix little bugs

--- backend/src/authentication/signup/request-handler.js
+++ backend/src/authentication/signup/request-handler.js
@@ -3,9 +3,9 @@ import { HttpResponse } from "./http-response"
 export const makeHandler = (signupService) => {
   const execute = async (request) => {
     try {
-      const token = await signupService.signup(request.user)
+      const response = await signupService.signup(request.user)
 
-      return HttpResponse.ok({ token })
+      return HttpResponse.ok(response.body)
     } catch (error) {
       return error.name === "InvalidParamError"
         ? HttpResponse.requestError({ error })
--- backend/src/authentication/signup/token-generator.js
+++ backend/src/authentication/signup/token-generator.js
@@ -2,7 +2,7 @@ import Jwt from "jsonwebtoken"
 
 export const makeTokenGenerator = () => {
   const generate = (userId) => {
-    return Jwt.sign(userId, process.env.JWT_SECRET)
+    return Jwt.sign({ userId }, process.env.JWT_SECRET)
   }
 
   return { generate }
