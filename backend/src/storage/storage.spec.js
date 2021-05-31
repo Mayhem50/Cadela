f73d2612aff7ab9c8cebb428498caa5891640fff
Benjamin Régnier <bregnier@jin.fr>
2021-05-31 10:41:10 +0200

    Refactor service constructor to get an object

--- backend/src/storage/storage.spec.js
+++ backend/src/storage/storage.spec.js
@@ -3,7 +3,7 @@ import { InvalidParamError, InternalError } from "../shared/errors"
 
 const RAW_DATA = {}
 
-const makeStoreService = (dataRepository) => {
+const makeStoreService = ({ dataRepository } = {}) => {
   const store = async (data) => {
     if (!dataRepository) {
       throw InternalError()
@@ -27,14 +27,14 @@ const dataRepository = makeRepository()
 
 describe("Storage", () => {
   it("Return success true if it success to store data", async () => {
-    const storageService = makeStoreService(dataRepository)
+    const storageService = makeStoreService({ dataRepository })
     const response = await storageService.store(RAW_DATA)
     expect(dataRepository.save).toBeCalledWith(RAW_DATA)
     expect(response.body.success).toBe(true)
   })
 
   it("Throw  an invalid parameter error if no data provided", async () => {
-    const storageService = makeStoreService(dataRepository)
+    const storageService = makeStoreService({ dataRepository })
     await expect(storageService.store()).rejects.toEqual(
       InvalidParamError("data")
     )
