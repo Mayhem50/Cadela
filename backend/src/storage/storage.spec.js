dcad16a1c2c0f90d46a93a0887cd7975f68b7a71
Benjamin Régnier <bregnier@jin.fr>
2021-05-31 10:38:00 +0200

    Make method async

--- backend/src/storage/storage.spec.js
+++ backend/src/storage/storage.spec.js
@@ -4,7 +4,7 @@ import { InvalidParamError } from "../shared/errors"
 const RAW_DATA = {}
 
 const makeStoreService = (dataRepository) => {
-  const store = (data) => {
+  const store = async (data) => {
     if (!data) {
       throw InvalidParamError("data")
     }
@@ -16,22 +16,24 @@ const makeStoreService = (dataRepository) => {
 }
 
 const makeRepository = () => {
-  const save = jest.fn((data) => {})
+  const save = jest.fn(async (data) => {})
   return { save }
 }
 
 const dataRepository = makeRepository()
 
 describe("Storage", () => {
-  it("Return success true if it success to store data", () => {
+  it("Return success true if it success to store data", async () => {
     const storageService = makeStoreService(dataRepository)
-    const response = storageService.store(RAW_DATA)
+    const response = await storageService.store(RAW_DATA)
     expect(dataRepository.save).toBeCalledWith(RAW_DATA)
     expect(response.body.success).toBe(true)
   })
 
-  it("Throw  an invalid parameter error if no data provided", () => {
+  it("Throw  an invalid parameter error if no data provided", async () => {
     const storageService = makeStoreService()
-    expect(storageService.store).toThrow(InvalidParamError("data"))
+    await expect(storageService.store()).rejects.toEqual(
+      InvalidParamError("data")
+    )
   })
 })
