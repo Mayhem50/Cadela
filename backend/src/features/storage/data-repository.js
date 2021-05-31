ac353e00c4366306afca6cf46cb3474eeb9959a8
Benjamin Régnier <bregnier@jin.fr>
2021-05-31 11:36:37 +0200

    Fix typo in data repo

--- backend/src/features/storage/data-repository.js
+++ backend/src/features/storage/data-repository.js
@@ -1,10 +1,10 @@
 const makeDataRepository = (client) => {
-  return (store = async (userId, data) => {
+  const store = async (userId, data) => {
     const collection = await client
       .db(process.env.DB_NAME)
       .collection(`${userId}-datas`)
     await collection.insertOne(data)
-  })
+  }
 
   return { store }
 }
