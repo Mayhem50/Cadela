a04768ddac9a743f656ec3eddfc881119d8e629b
Benjamin Régnier <bregnier@jin.fr>
2021-05-31 10:36:50 +0200

    Add repository to success test

--- backend/src/storage/storage.spec.js
+++ backend/src/storage/storage.spec.js
@@ -1,20 +1,32 @@
+import { jest, beforeEach } from "@jest/globals"
 import { InvalidParamError } from "../shared/errors"
 
-const makeStoreService = () => {
+const RAW_DATA = {}
+
+const makeStoreService = (dataRepository) => {
   const store = (data) => {
     if (!data) {
       throw InvalidParamError("data")
     }
+    dataRepository.save(data)
     return { body: { success: true } }
   }
 
   return { store }
 }
 
+const makeRepository = () => {
+  const save = jest.fn((data) => {})
+  return { save }
+}
+
+const dataRepository = makeRepository()
+
 describe("Storage", () => {
   it("Return success true if it success to store data", () => {
-    const storageService = makeStoreService()
-    const response = storageService.store({})
+    const storageService = makeStoreService(dataRepository)
+    const response = storageService.store(RAW_DATA)
+    expect(dataRepository.save).toBeCalledWith(RAW_DATA)
     expect(response.body.success).toBe(true)
   })
 
