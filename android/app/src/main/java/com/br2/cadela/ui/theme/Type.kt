a123eff15fdb3a99cc6472ffa01b81e6c23d1ce5
Benjamin REGNIER <regnier.ben@gmail.com>
2021-06-03 23:55:15 +0200

    Change default typo

--- android/app/src/main/java/com/br2/cadela/ui/theme/Type.kt
+++ android/app/src/main/java/com/br2/cadela/ui/theme/Type.kt
@@ -1,24 +1,31 @@
 package com.br2.cadela.ui.theme
 
+import android.graphics.Typeface
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.br2.cadela.R
 
+
+val FontFamily.Companion.Epilogue: FontFamily
+    get() = FontFamily(Font(R.font.epilogue))
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Epilogue,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Epilogue,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
+    /* Other default text styles to override
+
     caption = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
