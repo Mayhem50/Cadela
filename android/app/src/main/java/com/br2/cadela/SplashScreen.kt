e845016787002782af566a66292487c64aab889e
Benjamin REGNIER <regnier.ben@gmail.com>
2021-06-04 00:11:09 +0200

    Add App name to SplashScreen

--- android/app/src/main/java/com/br2/cadela/SplashScreen.kt
+++ android/app/src/main/java/com/br2/cadela/SplashScreen.kt
@@ -3,14 +3,22 @@ package com.br2.cadela
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import com.br2.cadela.ui.theme.Red200
@@ -45,6 +53,13 @@ fun SplashScreen(navController: NavController) {
                     .height(120f.dp),
                 alignment = Alignment.Center,
                 alpha = infinitelyAnimatedFloat.value
+            )
+            Text(
+                text = stringResource(id = R.string.app_name).toUpperCase(Locale.current),
+                fontSize = 28f.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier.padding(10f.dp)
             )
         }
     }
