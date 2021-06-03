8c554aded21e6a0b1781cbe8f521da11e8505c70
Benjamin REGNIER <regnier.ben@gmail.com>
2021-06-03 23:04:52 +0200

    Redesign SplashScreen

--- android/app/src/main/java/com/br2/cadela/SplashScreen.kt
+++ android/app/src/main/java/com/br2/cadela/SplashScreen.kt
@@ -1,36 +1,53 @@
 package com.br2.cadela
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
+import com.br2.cadela.ui.theme.Red200
+import com.br2.cadela.ui.theme.Red500
 import kotlinx.coroutines.*
 
 @Composable
 fun SplashScreen(navController: NavController) {
     val infinitelyAnimatedFloat = animatedLoop(800, 0.65f, 1f)
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription = null,
-            Modifier
-                .width(Dp(120f))
-                .height(Dp(120f)),
-            alignment = Alignment.Center,
-            alpha = infinitelyAnimatedFloat.value
+    Box(
+        modifier = Modifier.background(
+            brush = Brush.linearGradient(
+                listOf(
+                    Red500,
+                    Red200
+                ), start = Offset.Zero, end = Offset(1f, 1f)
+            )
         )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_without_gradient),
+                contentDescription = null,
+                Modifier
+                    .width(Dp(120f))
+                    .height(Dp(120f)),
+                alignment = Alignment.Center,
+                alpha = infinitelyAnimatedFloat.value
+            )
+        }
     }
+
     waitBeforeNavigate(5000, navController)
 }
 
--- android/app/src/main/java/com/br2/cadela/ui/theme/Color.kt
+++ android/app/src/main/java/com/br2/cadela/ui/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Red500 = Color(0xFFA31E00)
+val Red200 = Color(0xFFFE1211)
+val Red700 = Color(0xFFA70000)
