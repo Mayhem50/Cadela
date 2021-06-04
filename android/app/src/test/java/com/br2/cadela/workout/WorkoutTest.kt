31f7377916e90afa45509e0ece64bdfb51a78d12
Benjamin REGNIER <regnier.ben@gmail.com>
2021-06-04 10:57:20 +0200

    Run second program is B over or equal 4 for 1st level test

--- android/app/src/test/java/com/br2/cadela/workout/WorkoutTest.kt
+++ android/app/src/test/java/com/br2/cadela/workout/WorkoutTest.kt
@@ -38,7 +38,7 @@ class WorkoutTest {
 
     @Test
     fun `When session result on test session is under 4 for exercise B next session wil be First Program`() {
-        val sessionResult = SessionResult()
+        val sessionResult = SessionResult(listOf(Exercise("B", 3)))
         val session = sut.createNewSession(sessionResult)
 
         assertEquals("1st Program", session.name)
@@ -51,32 +51,47 @@ class WorkoutTest {
             session.restsBetweenExercises.stream().map { it.duration }.toList()
         )
     }
+
+    @Test
+    fun `When session result on test session is over or equal 4 for exercise B next session wil be First Program`() {
+        val sessionResult = SessionResult(listOf(Exercise("B", 4)))
+        val session = sut.createNewSession(sessionResult)
+
+        assertEquals("2nd Program", session.name)
+        assertEquals(
+            listOf("B", "A1", "D", "C1", "E", "F", "G", "K2"),
+            session.exercises.stream().map { it.name }.toList()
+        )
+        assertEquals(
+            List(7) { 120 },
+            session.restsBetweenExercises.stream().map { it.duration }.toList()
+        )
+    }
 }
 
 class WorkoutService {
     fun createNewSession(sessionResult: SessionResult? = null): Session {
-        if(sessionResult != null) {
-            return Session(
-                name = "1st Program",
-                exercises = listOf(
-                    Exercise(name = "A"),
-                    Exercise(name = "D"),
-                    Exercise(name = "C1"),
-                    Exercise(name = "E"),
-                    Exercise(name = "F"),
-                    Exercise(name = "G"),
-                    Exercise(name = "K2")
-                ),
-                restsBetweenExercises = listOf(
-                    Rest(duration = 120),
-                    Rest(duration = 120),
-                    Rest(duration = 120),
-                    Rest(duration = 120),
-                    Rest(duration = 120),
-                    Rest(duration = 120)
-                ))
+        sessionResult?.let {
+            val repetitions = sessionResult.exercises.find { it.name == "B" }?.repetitions ?: 0
+            return if (repetitions < 4) {
+                Session.FIRST_PROGRAM
+            } else {
+                Session.SECOND_PROGRAM
+            }
         }
-        return Session(
+
+        return Session.FIRST_LEVEL_TEST
+    }
+}
+
+data class SessionResult(val exercises: List<Exercise>)
+data class Session(
+    val name: String,
+    val exercises: List<Exercise>,
+    val restsBetweenExercises: List<Rest>
+) {
+    companion object {
+        val FIRST_LEVEL_TEST = Session(
             name = "1st Level Test",
             exercises = listOf(
                 Exercise(name = "A"),
@@ -90,15 +105,51 @@ class WorkoutService {
                 Rest(duration = 180)
             )
         )
+        val FIRST_PROGRAM = Session(
+            name = "1st Program",
+            exercises = listOf(
+                Exercise(name = "A"),
+                Exercise(name = "D"),
+                Exercise(name = "C1"),
+                Exercise(name = "E"),
+                Exercise(name = "F"),
+                Exercise(name = "G"),
+                Exercise(name = "K2")
+            ),
+            restsBetweenExercises = listOf(
+                Rest(duration = 120),
+                Rest(duration = 120),
+                Rest(duration = 120),
+                Rest(duration = 120),
+                Rest(duration = 120),
+                Rest(duration = 120)
+            )
+        )
+
+        val SECOND_PROGRAM = Session(
+            name = "2nd Program",
+            exercises = listOf(
+                Exercise(name = "B"),
+                Exercise(name = "A1"),
+                Exercise(name = "D"),
+                Exercise(name = "C1"),
+                Exercise(name = "E"),
+                Exercise(name = "F"),
+                Exercise(name = "G"),
+                Exercise(name = "K2")
+            ),
+            restsBetweenExercises = listOf(
+                Rest(duration = 120),
+                Rest(duration = 120),
+                Rest(duration = 120),
+                Rest(duration = 120),
+                Rest(duration = 120),
+                Rest(duration = 120),
+                Rest(duration = 120)
+            )
+        )
     }
 }
 
-class SessionResult {}
-data class Session(
-    val name: String,
-    val exercises: List<Exercise>,
-    val restsBetweenExercises: List<Rest>
-)
-
 data class Rest(val duration: Int)
-data class Exercise(val name: String)
+data class Exercise(val name: String, val repetitions: Int = 0)
