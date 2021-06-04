b59b2310a0ac2b3defaa4cee955a38e7833b9796
Benjamin REGNIER <regnier.ben@gmail.com>
2021-06-04 23:04:57 +0200

    Session result is A2-8 A4-10, next session will replace A4 by A5

--- android/app/src/test/java/com/br2/cadela/workout/FirstLevelSessionFirstProgramTest.kt
+++ android/app/src/test/java/com/br2/cadela/workout/FirstLevelSessionFirstProgramTest.kt
@@ -286,6 +286,24 @@ class FirstLevelSessionFirstProgramTest : WorkoutTestBase() {
     }
 
     @Test
+    fun `Session result is A2-8 A4-10, next session will replace A4 by A5`() {
+        val sessionResult = SessionResult(
+            name = "1st Program",
+            exercises = listOf(
+                Exercise(name = "A4", series = Series(2, listOf(10))),
+                Exercise(name = "A2", series = Series(2, listOf(8))),
+                Exercise(name = "D", series = Series(2)),
+                Exercise(name = "C1", series = Series(2)),
+                Exercise(name = "E", series = Series(2)),
+                Exercise(name = "F", series = Series(2)),
+                Exercise(name = "G", series = Series(2)),
+                Exercise(name = "K2", series = Series(2)))
+        )
+        val session = sut.createNewSession(sessionResult)
+        assertSession(session, "A5", "A2", "D", "C1", "E", "F", "G", "K2")
+    }
+
+    @Test
     fun `Session result is A2-8 C4-12, next session will add A3 and replace C4 by C5`() {
         val sessionResult = SessionResult(
             name = "1st Program",
