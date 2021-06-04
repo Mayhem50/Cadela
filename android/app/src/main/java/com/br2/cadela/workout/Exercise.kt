076af9a4547e6bb7ec462f879c03373a794da4fd
Benjamin REGNIER <regnier.ben@gmail.com>
2021-06-04 11:00:51 +0200

    Extract data classes

---
+++ android/app/src/main/java/com/br2/cadela/workout/Exercise.kt
@@ -0,0 +1,3 @@
+package com.br2.cadela.workout
+
+data class Exercise(val name: String, val repetitions: Int = 0)
---
+++ android/app/src/main/java/com/br2/cadela/workout/Rest.kt
@@ -0,0 +1,3 @@
+package com.br2.cadela.workout
+
+data class Rest(val duration: Int)
