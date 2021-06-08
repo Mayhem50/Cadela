1f01dadaca14ab58e0aa0b6ded82a9e7314b92dd
Benjamin REGNIER <regnier.ben@gmail.com>
2021-06-08 15:41:32 +0200

    Improve SigninView

--- android/app/src/main/java/com/br2/cadela/authentication/signin/SigninView.kt
+++ android/app/src/main/java/com/br2/cadela/authentication/signin/SigninView.kt
@@ -1,14 +1,20 @@
 package com.br2.cadela.authentication.signin
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.intl.Locale
@@ -21,6 +27,7 @@ import com.br2.cadela.shared.buildPopupToCurrent
 import com.br2.cadela.shared.navigateStringResource
 import kotlinx.coroutines.launch
 
+@ExperimentalComposeUiApi
 @Composable
 fun SigninView(navController: NavController) {
     val vm = AuthenticationModule.signinVm
@@ -30,15 +37,29 @@ fun SigninView(navController: NavController) {
     val error by vm.error.observeAsState("")
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val passwordFocusRequester = FocusRequester()
+
 
     Scaffold(scaffoldState = scaffoldState) {
+        fun signin() {
+            keyboardController?.hide()
+            vm.signin(email, password) {
+                navController.navigateStringResource(
+                    R.string.nav_workout_home,
+                    NavOptions.Builder().buildPopupToCurrent(navController)
+                )
+            }
+        }
+
         if (error.isNotEmpty()) {
             scope.launch {
                 scaffoldState.snackbarHostState.showSnackbar(
                     message = error,
                     duration = SnackbarDuration.Short,
-                    actionLabel = null
+                    actionLabel = null,
                 )
+                vm.clearError()
             }
         }
         Column(
@@ -50,29 +71,34 @@ fun SigninView(navController: NavController) {
                 .fillMaxHeight()
         ) {
             OutlinedTextField(
+                keyboardActions = KeyboardActions(onNext = { passwordFocusRequester.requestFocus() }),
                 label = { Text(stringResource(id = R.string.email)) },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
                 value = email,
                 onValueChange = { email = it },
                 modifier = Modifier.padding(8.0f.dp)
             )
             OutlinedTextField(
+                keyboardActions = KeyboardActions(onDone = { signin() }),
                 label = { Text(stringResource(id = R.string.password)) },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
                 visualTransformation = PasswordVisualTransformation(),
                 value = password,
                 onValueChange = { password = it },
-                modifier = Modifier.padding(8.0f.dp),
+                modifier = Modifier
+                    .padding(8.0f.dp)
+                    .focusRequester(passwordFocusRequester),
             )
             Box(modifier = Modifier.height(124f.dp)) {
                 if (!loading) Button(
                     onClick = {
-                        vm.signin(email, password) {
-                            navController.navigateStringResource(
-                                R.string.nav_workout_home,
-                                NavOptions.Builder().buildPopupToCurrent(navController)
-                            )
-                        }
+                        signin()
                     },
                     modifier = Modifier.padding(8.0f.dp),
                 ) {
