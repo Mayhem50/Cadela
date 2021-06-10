c1a4f449d1b95abb8461a6c4f8a5c450ae174798
Benjamin REGNIER <regnier.ben@gmail.com>
2021-06-10 11:30:16 +0200

    Refacto

--- android/app/src/main/java/com/br2/cadela/workout/WorkoutHomeView.kt
+++ android/app/src/main/java/com/br2/cadela/workout/WorkoutHomeView.kt
@@ -15,9 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
