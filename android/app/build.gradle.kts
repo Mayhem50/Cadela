f744ffad62a369eb8f1853b25da8fca3e30f9938
Benjamin REGNIER <regnier.ben@gmail.com>
2021-06-16 10:15:39 +0200

    Update IDE & project files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -60,11 +60,15 @@ dependencies {
     implementation("androidx.compose.material:material-icons-core:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material-icons-extended:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["compose_version"]}")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha07")
     implementation("androidx.navigation:navigation-compose:2.4.0-alpha02")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.0-beta01")
+
     implementation("com.google.accompanist:accompanist-insets-ui:0.11.1")
+    implementation("com.google.accompanist:accompanist-pager:0.11.1")
+    implementation ("com.google.accompanist:accompanist-pager-indicators:0.11.1")
 
     val room_version = "2.3.0"
 
